1927969c6c451b87f94ef34ce63a2cfecf181d93
Niclas-L <lemstromniclas@gmail.com>
2023-07-25 15:55:58 +0300

    Added list of questions

--- admin.py
+++ admin.py
@@ -14,3 +14,16 @@ def delete_user(user_id):
     sql = text("DELETE FROM users WHERE id=:id")
     db.session.execute(sql, {"id": user_id})
     db.session.commit()
+
+
+def get_questions():
+    sql = text("SELECT question_id, question FROM questions")
+    result = db.session.execute(sql)
+    questions = result.fetchall()
+    return questions
+
+
+def delete_question(question_id):
+    sql = text("DELETE FROM questions WHERE question_id=:id")
+    db.session.execute(sql, {"id": question_id})
+    db.session.commit()
